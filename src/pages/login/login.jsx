a1f05cf9a54c68bcaaa9d78156d607b933676edc
Talles Dias <tallesdelfino1@gmail.com>
2022-11-21 12:25:48 -0300

    insert image

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import image from "../../assets/Group.svg"
 export const Login = () => {
     return (
-        <div></div>
+        <Container>
+            <Row>
+                <Col sm={8}><img src={image} /></Col>
+                <Col sm={4}></Col>
+            </Row>
+        </Container>
     );
 }
