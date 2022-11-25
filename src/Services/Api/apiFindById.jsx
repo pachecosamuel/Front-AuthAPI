87c733bf5681b8cf2d469be8088a3412b0cb86af
Talles Dias <tallesdelfino1@gmail.com>
2022-11-25 17:52:25 -0300

    Finalizando profile

---
+++ src/Services/Api/apiFindById.jsx
@@ -0,0 +1,24 @@
+import { api } from "./apiConnection";
+
+
+const FindById = async (id, token) => {
+
+    try {
+        const user = await api.get(`User/${id}`,
+            // {
+            //     headers: {
+            //         Authorization: `Bearer ${token}`,
+            //         "Content-Type": "application/json",
+            //         Accept: "application/json",
+            //     },
+            // }
+        );
+        if (user.status === 200) return user;
+        return false;
+
+    } catch (error) {
+        alert(error.response.headers.error);
+    }
+
+}
+export { FindById };
--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { AuthenticationContext } from "../../Services/Context/contextToken";
 import { PageContainer } from "../../components/page-container/style";
 import ContentPageContainer from "../../components/content-page-container";
 import HeaderPageComponent from "../../components/header-page";
@@ -7,8 +7,21 @@ import Col from 'react-bootstrap/Col';
 import AddressCard from "../../components/profileCard/addressCard";
 import { BiUserCircle } from "react-icons/bi";
 import InformationCard from "../../components/profileCard/informationCard/index";
-
+import React, { useContext, useEffect, useState } from 'react';
+import { FindById } from "../../Services/Api/apiFindById";
+import { Await } from "react-router-dom";
 export function Profile() {
+
+    const { user } = useContext(AuthenticationContext);
+    const [newUser, setNewUser] = useState({});
+    useEffect(() => {
+        Find(user.id, user.token);
+    }, []);
+
+    const Find = async (id, token) => {
+        const result = await FindById(id, token);
+        setNewUser(result.data.data);
+    };
     return (
         <PageContainer>
             <HeaderPageComponent title='Perfil' icon={<BiUserCircle />} />
@@ -16,9 +29,9 @@ export function Profile() {
             <ContentPageContainer>
                 <Row>
                     <Col>
-                        <InformationCard />
+                        <InformationCard user={newUser} />
                         <hr />
-                        <AddressCard />
+                        <AddressCard user={newUser} />
                     </Col>
                 </Row>
             </ContentPageContainer>
