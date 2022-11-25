39d7449ec3dfa1413d72cb11e7f0d36b4d44a7eb
Pedro Rodrigues <pedrodesarodrigues@gmail.com>
2022-11-25 15:39:22 -0300

    loading component

--- src/components/loading/index.jsx
+++ src/components/loading/index.jsx
@@ -1,8 +1,22 @@
+import { Spinner } from "react-bootstrap"
 
 export const LoadingComponent = () => {
     return (
-        <h1>
-            LOADING...
-        </h1>
+        <div style={{
+            height: '100vh',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+        }}>
+            <span>
+                <Spinner
+                    as="span"
+                    animation="border"
+                    size="sm"
+                    role="status"
+                    aria-hidden="true"
+                /> Carregando...
+            </span>
+        </div>
     )
 }
