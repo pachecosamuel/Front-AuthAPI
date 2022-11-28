a012f7bf19d4a064d7945b4edc2d47cfd6651a1d
Pedro Rodrigues <pedrodesarodrigues@gmail.com>
2022-11-28 19:31:09 -0300

    changing toast to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,17 @@ import { Root } from './routes/root'
 import "./css/global.css";
 import "./css/css-config/reset.css";
 import { AuthenticationProvider } from './Services/Context/contextToken';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
 function App() {
     return (
         <AuthenticationProvider>
+            <ToastContainer
+                position='top-right'
+                hideProgressBar
+                theme='colored'
+            />
             <Root />
         </AuthenticationProvider>
     )
--- src/pages/cadastro/index.jsx
+++ src/pages/cadastro/index.jsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
 
-import { Container } from "react-bootstrap"
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
@@ -9,19 +7,15 @@ import { AiOutlineUserAdd } from 'react-icons/ai';
 import { PageContainer } from "../../components/page-container/style";
 import HeaderPageComponent from "../../components/header-page";
 import ContentPageContainer from "../../components/content-page-container";
-import SearchComponent from "../../components/search";
-import TableComponent from "../../components/table";
-import PaginationComponent from "../../components/pagination";
 import FormularioCadastroComponent from "../../components/formCadastro";
 
 import { ContainerTablePageStyle } from "./style"
 
 export const Cadastro = () => {
-    
     return (
         <PageContainer>
             <HeaderPageComponent title='Cadastro' icon={<AiOutlineUserAdd />} />
-            
+
             <ContentPageContainer>
                 <ContainerTablePageStyle>
                     <Row>
