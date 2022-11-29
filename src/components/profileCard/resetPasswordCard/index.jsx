bd0ea00c6f2dac70d87d7823b1e2bd3ea1b3dfce
Talles Dias <tallesdelfino1@gmail.com>
2022-11-29 09:48:29 -0300

    ajustando validaçao

--- src/components/profileCard/resetPasswordCard/index.jsx
+++ src/components/profileCard/resetPasswordCard/index.jsx
@@ -8,8 +8,24 @@ import Modal from 'react-bootstrap/Modal';
 
 function PasswordCard() {
     const [show, setShow] = useState(false);
+    const [password, setPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("");
     const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const [validated, setValidated] = useState(false)
+
+
+
+    const handleShow = (e) => {
+        e.preventDefault();
+        setValidated(true)
+        setShow(true);
+        console.log("senha : " + password);
+        console.log("newsenha : " + newPassword);
+        console.log("confirsenha : " + passwordConfirm);
+    }
+
+
 
 
     //Falta consumir a Api
@@ -24,24 +40,30 @@ function PasswordCard() {
                 <Accordion.Item eventKey="0">
                     <Accordion.Header>Redefinir Senha</Accordion.Header>
                     <Accordion.Body >
-                        <Form.Group controlId="formGridPassword">
-                            <Form.Label>Senha atual</Form.Label>
-                            <Form.Control type="password" placeholder="Digite sua senha..." />
-                        </Form.Group>
-                        <Form.Group controlId="formGridPassword">
-                            <Form.Label>Nova senha</Form.Label>
-                            <Form.Control type="password" placeholder="Nova senha..." />
-                        </Form.Group>
-                        <Form.Group controlId="formGridPassword">
-                            <Form.Label>Confirme sua senha</Form.Label>
-                            <Form.Control type="password" placeholder="Confirme sua senha..." />
-                        </Form.Group>
+                        <Form noValidate validated={validated}>
+                            <Form.Group controlId="formGridPassword">
+                                <Form.Label>Senha atual</Form.Label>
+                                <Form.Control type="password" placeholder="Digite sua senha..."
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required
+                                />
+                            </Form.Group>
+                            <Form.Group controlId="formGridPassword">
+                                <Form.Label>Nova senha</Form.Label>
+                                <Form.Control type="password" placeholder="Nova senha..." value={newPassword} onChange={(e) => setNewPassword(e.target.value)} required />
+                            </Form.Group>
+                            <Form.Group controlId="formGridPassword" >
+                                <Form.Label>Confirme sua senha</Form.Label>
+                                <Form.Control type="password" placeholder="Confirme sua senha..." value={passwordConfirm} onChange={(e) => setPasswordConfirm(e.target.value)} required />
+                            </Form.Group>
+                        </Form>
                         <div style={{ marginTop: "2rem" }}>
                             <ButtonComponent
                                 tamanho="10rem"
                                 bgColor="#01a998"
                                 textColor="#FFF"
-                                acao={handleShow}
+                                acao={(e) => handleShow(e)}
                             >
                                 Redefinir
                             </ButtonComponent>
