97d21803391754b3f03cd1a92a06bee2402af5d9
Pedro Rodrigues <pedrodesarodrigues@gmail.com>
2022-12-02 10:38:54 -0300

    user register form schema validation complete

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -115,6 +115,7 @@ function SidebarComponent() {
               />
             ) : (
               <BsArrowBarLeft
+                title="Reduzir"
                 onClick={() => setSideBarCollapse(!sideBarCollapse)}
               />
             )}
--- src/utils/validation/regexValidations.js
+++ src/utils/validation/regexValidations.js
@@ -1,5 +1,1 @@
-// const phoneRegex = /^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/
-// const onlyNumbersRegex = /[0-9]/
-// const cpfRegex = /[0-9]{11}/
-
 export const noNumbersRegex = /^([^0-9]*)$/
